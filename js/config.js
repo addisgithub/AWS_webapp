344f77aa9ff67e64bd5dd774d7b271611935fab0
addisgithub <addis.bogale@ucollege.edu>
2021-02-12 18:52:01 +0000

    updated config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_AJL4WnsIC', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '41equt8r66j7gg2u9iq7fuflrk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '/us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.uq7fuflrks-west-2.amazonaws.com/prod',
     }
 };
